ef869ba0eb52e8d19cbbaa02185b3252e4143b02
avost <noasueur88@gmail.com>
2025-10-06 21:25:37 +0200

    main_website.py

--- main_website.py
+++ main_website.py
@@ -153,5 +153,5 @@ if __name__ == '__main__':
     # On ne démarre QUE le serveur web
     from waitress import serve
     port = int(os.environ.get("PORT", 5001))
-    print(f">>> Démarrage du serveur web Waitress SEUL sur le port {port}")
     serve(app, host="0.0.0.0", port=port)
+    print(f">>> Démarrage du serveur web Waitress SEUL sur le port {port}")
